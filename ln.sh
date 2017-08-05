2d282ae207d92a055b0e1fe33981f794c77ea8c8
tao <jht1983@gmail.com>
2017-08-05 19:36:43 +0800

    修改：     ln.sh 	修改：     readme.md

--- ln.sh
+++ ln.sh
@@ -1,4 +1,6 @@
 #/bin/bash
+mkdir ~/.i3 ~/bin
+
 ln -sf /home/tao/.rcfiles/zshrc ~/.zshrc
 ln -sf /home/tao/.rcfiles/bashrc ~/.bashrc
 ln -sf /home/tao/.rcfiles/bash_aliases ~/.bash_aliases
@@ -6,3 +8,9 @@ ln -sf /home/tao/.rcfiles/config ~/.i3/config
 ln -sf /home/tao/.rcfiles/i3status.conf ~/.i3status.conf
 ln -sf /home/tao/.rcfiles/vimrc ~/.vimrc
 
+ln -sf /home/tao/.rcfiles/hosts ~/bin/hosts
+ln -sf /home/tao/.rcfiles/debiancn.sh ~/bin/debiancn.sh
+ln -sf /home/tao/.rcfiles/dmenu-frecency ~/bin/dmenu-frecency
+ln -sf /home/tao/.rcfiles/unzipgbk ~/bin/unzipgbk
+ln -sf /home/tao/.rcfiles/ydcv ~/bin/ydcv
+ln -sf /home/tao/.rcfiles/start_conky_maia ~/bin/start_conky_maia
