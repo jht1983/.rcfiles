9b2d53d91c21faabd97a0e3b0b752bb9fbe9ef80
tao <jht1983@gmail.com>
2017-08-05 13:17:16 +0800

    新文件：   deb-task.sh 	修改：     config

---
+++ deb-task.sh
@@ -0,0 +1,1 @@
+apt install sudo git axel i3 j4-dmenu-desktop xorg aptitude vim ssh task-chinese-s  task-chinese-s-desktop  net-tools htop gnome-setting-daemon compton scrot volumeicon-alsa alsa-utils pcmanfm 
