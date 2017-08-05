7b4a0052cd98179b3088bba2c51f5f2542e32217
tao <jht1983@gmail.com>
2017-08-05 14:28:49 +0800

    修改：     deb-task.sh 	修改：     bash_aliases

--- deb-task.sh
+++ deb-task.sh
@@ -1,1 +1,5 @@
-apt install sudo git axel i3 j4-dmenu-desktop xorg aptitude vim ssh task-chinese-s  task-chinese-s-desktop  net-tools htop gnome-setting-daemon compton scrot volumeicon-alsa alsa-utils pcmanfm 
+apt install sudo zsh wget git axel i3 j4-dmenu-desktop xorg aptitude vim ssh task-chinese-s  task-chinese-s-desktop  net-tools htop gnome-setting-daemon compton scrot volumeicon-alsa alsa-utils pcmanfm tree nitrogen rxvt-unicode conky evince google-chrome powerline vim-gtk vim-addon-manager vim-python-jedi vim-youcompleteme proxychains nm-applet p7zip-full p7zip-rar feh mutt mc lightdm 
+
+
+ehco "deb https://repo.debiancn.org/ stretch main" >/etc/apt/sources.list.d/debiancn.list
+#其他常用软件：lantern goagent typora aynaptic onedrive pycharm Vundle wps-office mysql sqlite django sublime virtualbox electronic-wechat mousepad kate python-pip ipython wps-fonts foxitreader virtualbox-guest-dkms broadcom-sta-dkms
