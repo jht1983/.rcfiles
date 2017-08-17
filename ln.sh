c7902ca2b31efe81433f7088be7d7ac18b0d947f
jht1983 <jht1983@gmail.com>
2017-08-17 16:47:46 +0800

    Update ln.sh

--- ln.sh
+++ ln.sh
@@ -6,7 +6,7 @@ ln -sf /home/tao/.rcfiles/bashrc ~/.bashrc
 ln -sf /home/tao/.rcfiles/bash_aliases ~/.bash_aliases
 ln -sf /home/tao/.rcfiles/config ~/.i3/config
 ln -sf /home/tao/.rcfiles/i3status.conf ~/.i3status.conf
-ln -sf /home/tao/.rcfiles/vimrc ~/.vimrc
+#ln -sf /home/tao/.rcfiles/vimrc ~/.vimrc
 
 ln -sf /home/tao/.rcfiles/hosts ~/bin/hosts
 ln -sf /home/tao/.rcfiles/debiancn.sh ~/bin/debiancn.sh
