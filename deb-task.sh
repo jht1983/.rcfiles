ae16e708588e970d716188f4e67acbfa5b1bcef0
tao <jht1983@gmail.com>
2017-09-29 21:44:28 +0800

    修改：     bash_aliases 	修改：     config 	修改：     readme.md 	修改：     vimrc

--- deb-task.sh
+++ deb-task.sh
@@ -5,7 +5,7 @@ sudo dpkg -i debiancn-keyring_0~20161212_all.deb
 echo "deb http://repo.debiancn.org/ stretch main" |sudo tee /etc/apt/sources.list.d/debiancn.list
 sudo apt update
 
-sudo apt install zsh axel mlocate apt-transport-https i3 j4-dmenu-desktop xorg aptitude vim ssh task-chinese-s  task-chinese-s-desktop  net-tools htop gnome-settings-daemon compton scrot volumeicon-alsa alsa-utils pcmanfm tree nitrogen rxvt-unicode conky evince powerline vim-gtk vim-addon-manager vim-python-jedi vim-youcompleteme proxychains network-manager-gnome p7zip-full feh mutt mc lightdm xfce4-terminal python-pip clipit
+sudo apt install zsh axel mlocate apt-transport-https i3 j4-dmenu-desktop xorg aptitude vim ssh task-chinese-s  task-chinese-s-desktop  net-tools htop gnome-settings-daemon compton scrot volumeicon-alsa alsa-utils pcmanfm tree nitrogen rxvt-unicode conky evince powerline vim-gtk vim-addon-manager vim-python-jedi vim-youcompleteme proxychains network-manager-gnome p7zip-full feh mutt mc lightdm xfce4-terminal python-pip clipit pmount
 
 #sudo apt install wps-office wps-office-fonts google-chrome-stable virtualbox sublime-text-dev python-pip ipython electronic-wechat
 #sudo apt install broadcom-sta-dkms
