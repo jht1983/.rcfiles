edcccafc9795f4e369bb9fdc5373a48544f67858
jht1983 <jht1983@gmail.com>
2019-01-30 00:04:11 +0800

    Update debiancn.sh

--- debiancn.sh
+++ debiancn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update;
 sudo apt install apt-transport-https;
-sudo printf "deb https://mirrors.ustc.edu.cn/debiancn/ testing main" > /etc/apt/sources.list.d/debiancn.list;
+# sudo printf "deb https://mirrors.ustc.edu.cn/debiancn/ testing main" > /etc/apt/sources.list.d/debiancn.list;
 sudo wget https://mirrors.ustc.edu.cn/debiancn/debiancn-keyring_0~20161212_all.deb -O /tmp/debiancn-keyring.deb;
 sudo apt install /tmp/debiancn-keyring.deb;
 sudo apt update;
