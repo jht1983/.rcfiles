e509a3529f3083b7b2927ce9a0542017eb074627
jht1983 <jht1983@gmail.com>
2019-02-02 17:50:55 +0800

    修改：     deb-task.sh

--- deb-task.sh
+++ deb-task.sh
@@ -5,7 +5,8 @@ sudo dpkg -i debiancn-keyring_0~20161212_all.deb
 echo "deb https://mirrors.ustc.edu.cn/debiancn/ buster main" |sudo tee /etc/apt/sources.list.d/debiancn.list
 sudo apt update
 
-sudo apt-get install zsh axel mlocate apt-transport-https i3 j4-dmenu-desktop xorg aptitude vim ssh task-chinese-s  task-chinese-s-desktop  net-tools htop gnome-settings-daemon compton scrot volumeicon-alsa alsa-utils pcmanfm tree nitrogen rxvt-unicode conky evince powerline vim-gtk vim-addon-manager vim-python-jedi vim-youcompleteme proxychains network-manager-gnome p7zip-full feh mutt lightdm xfce4-terminal python-pip pmount mousepad putty
+sudo apt-get install zsh axel mlocate apt-transport-https i3 j4-dmenu-desktop xorg aptitude vim ssh task-chinese-s  task-chinese-s-desktop  net-tools htop gnome-settings-daemon compton scrot volumeicon-alsa alsa-utils pcmanfm tree nitrogen rxvt-unicode conky evince powerline vim-gtk vim-addon-manager vim-python-jedi vim-youcompleteme proxychains network-manager-gnome p7zip-full feh mutt lightdm python-pip pmount mousepad putty cutecom minicom picocom deepin-terminal 
+
 
 #sudo apt install wps-office wps-office-fonts google-chrome-stable virtualbox python-pip ipython electronic-wechat mc clipit
 #sudo apt install synaptic
